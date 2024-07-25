247f8bccf16c05720d874c70c7f90635a0c900c5
BogdanLi <li.bogdan44@gmail.com>
2024-07-25 15:21:34 +0500

    Added pipe validating

--- src/dto/extensions/create-extension.dto.ts
+++ src/dto/extensions/create-extension.dto.ts
@@ -1,10 +1,14 @@
-export class CreateExtensionDto {
-  id: number;
+import { Length } from 'class-validator';
 
+export class CreateExtensionDto {
+  @Length(3, 255)
   name: string;
 
+  @Length(3, 255)
   description_ru: string;
+  @Length(3, 255)
   description_en: string;
 
+  @Length(3, 255)
   link: string;
 }
